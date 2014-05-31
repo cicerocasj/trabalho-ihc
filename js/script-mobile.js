c7a140c1bf17e41d7b2d63b9dc66ba93702cdc6f
Raphael Willian <raphael.wcosta@gmail.com>
2014-05-31 19:41:46 -0300

    swipe

--- js/script-mobile.js
+++ js/script-mobile.js
@@ -1,8 +1,6 @@
 $('#home').on('swiperight', function(){
-    $("#navpanel-left").panel("open");
+    $( "#navpanel-left" ).panel( "open");
 });
 $('#home').on('swipeleft', function(){
-    //if ($('#navpanel').panel().attr('class').indexOf('ui-panel-open') == -1) {
-        $("#navpanel").panel("open");
-    //}
+    $("#navpanel").panel("open");
 });
