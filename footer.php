10724c8105fd61c0960db1242067a53b6c2aa949
Raphael Willian <raphael.wcosta@gmail.com>
2014-05-31 19:43:02 -0300

    swipe master funcionando

--- footer.php
+++ footer.php
@@ -40,11 +40,10 @@
 
 </div>
 <!-- Jquery Mobile -->
-<script type="text/javascript" src="../js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="../js/jquery.js"></script>
 <script type="text/javascript" src="../js/jquery.mobile-1.3.1.min.js"></script>
 <script type="text/javascript" src="../js/script-mobile.js"></script>
-<script type="text/javascript" src="../js/jquery.js"></script>
-<script type="text/javascript" src="../js/groundwork.all.js"></script>
+
 <!--<script type="text/javascript" src="../js/scripts.js"></script>-->
 
 <!-- Modernizr -->
