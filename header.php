e2f71cdf6babdf6ed71396add5fe6ad4d2bc510b
cicerocasj <cicerocasj@gmail.com>
2014-05-30 22:58:47 -0300

    organizando as paginas

--- header.php
+++ header.php
@@ -1,60 +1,42 @@
-<!doctype html>
-<!--[if lt IE 7]>      <html lang="pt-BR" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html lang="pt-BR" class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html lang="pt-BR" class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html lang="pt-BR" class="no-js" xmlns="http://www.w3.org/1999/html"> <!--<![endif]-->
+<!DOCTYPE html>
+<html lang="pt-BR">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-    <title>Nome do projeto</title>
-    <!-- framework css -->
-    <!--[if gt IE 9]><!-->
-    <link type="text/css" rel="stylesheet" href="../css/groundwork.css">
-    <!--<![endif]-->
-    <!--[if lte IE 9]>
-    <link type="text/css" rel="stylesheet" href="../css/groundwork-core.css">
-    <link type="text/css" rel="stylesheet" href="../css/groundwork-type.css">
-    <link type="text/css" rel="stylesheet" href="../css/groundwork-ui.css">
-    <link type="text/css" rel="stylesheet" href="../css/groundwork-anim.css">
-    <link type="text/css" rel="stylesheet" href="../css/groundwork-ie.css">
-    <![endif]-->
-    <link type="text/css" rel="stylesheet" href="../css/jcarousel.responsive.css">
-    <link type="text/css" rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/grafico_noticias/grafico_noticia.css"/>
+    <title>Mortalidade no Trânsito</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="stylesheet" href="../css/jquery.mobile-1.3.1.min.css" />
+    <link rel="stylesheet" href="../css/styles.css" />
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <script type="text/javascript" src="../js/graficos_noticias/amcharts.js"></script>
+    <script type="text/javascript" src="../js/graficos_noticias/serial.js"></script>
+    <script type="text/javascript" src="../js/graficos_noticias/dark.js"></script>
 </head>
 <body>
-<section>
-    <header>
-        <nav role="navigation" class="nav charcoal mobile" title="Menu">
-            <ul role="menubar">
-                <li>
-                    <a href="../home"><i class="icon-home"></i> Home</a>
-                </li>
-                <li role="menu">
-                    <button><i class="icon-bar-chart"></i> Noticias</button>
-                    <ul role="menu" aria-expanded="false">
-                        <li>
-                            <a href="../evolucao-dos-acidentes-de-transito" title="Evolução dos acidentes de trânsito 1980/2011">Evolução dos acidentes de trânsito 1980/2011</a>
-                        </li>
-                        <li>
-                            <a href="../evolucao-por-categoria-de-acidente" title="Evolução por categoria de acidente">Evolução por categoria de acidente</a>
-                        </li>
-                        <li>
-                            <a href="../frota-veicular-e-mortalidade-no-transito" title="Frota veicular e mortalidade no trânsito">Frota veicular e mortalidade no trânsito</a>
-                        </li>
-                        <li>
-                            <a href="../internacoes-hospitalares-por-acidentes-de-transito" title="Internações hospitalares por acidentes de trânsito">Internações hospitalares por acidentes de trânsito</a>
-                        </li>
-                        <li>
-                            <a href="../mortalidade-no-transito-das-capitais" title="Mortalidade no trânsito das capitais">Mortalidade no trânsito das capitais</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="../contato" title="Contato"><i class="icon-user"></i> Contato</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+<div data-role="page" id="home" data-theme="c">
+    <div data-role="panel" id="navpanel-left" data-theme="c"
+         data-display="overlay" data-position="left">
+        <div data-role="controlgroup" data-corners="false">
+            <a href="#" data-role="button">Home</a>
+            <a href="#" data-role="button">Contato</a>
+        </div>
+    </div>
+    <div data-role="panel" id="navpanel" data-theme="c"
+         data-display="overlay" data-position="right">
+        <div data-role="controlgroup" data-corners="false">
+            <a href="../evolucao-dos-acidentes-de-transito/" data-role="button">Evolução dos acidentes de trânsito</a>
+            <a href="#" data-role="button">Evolução por categoria de acidente</a>
+            <a href="#" data-role="button">Frota veicular e mortalidade no trânsito</a>
+            <a href="#" data-role="button">Internações hospitalares por acidentes de transito</a>
+            <a href="#" data-role="button">Mortalidade no trânsito das capitais</a>
+        </div>
+    </div>
+    <div id="header" data-role="header" data-theme="a">
+        <a id="bars-button-left" data-icon="bars"  class="ui-btn-left menu-mobile" style="margin-top:10px;" href="#navpanel-left">Menu</a>
+        <a id="bars-button" data-icon="bars"  class="ui-btn-right menu-mobile" style="margin-top:10px;" href="#navpanel">Ultimas noticias</a>
+        <div class="menu-tablet">
+            <a href="#">Home</a>
+            <a href="#">Noticias</a>
+            <a href="#">Contato</a>
+        </div>
+    </div>
+    <div id="index" data-role="content">
