14f21474eabcdabcdd518e32a6768d3f16007a21
cicerocasj <cicerocasj@hotmail.com>
2014-06-01 15:05:31 -0300

    melhorado os contatos

--- contato/index.php
+++ contato/index.php
@@ -1,28 +1,22 @@
 <?php include "../header.php"; ?>
     <div class="ui-grid-b my-breakpoint" id="contact">
-        <div class="ui-block-a">
-            <div class="notice box gray square white">
-                <h4 data-compression="7" data-max="20" class="align-center zero">
-                    Cícero Alves
-                </h4>
-                <p><img src="../images/cicero.jpg" alt="Cícero Alves" title="Cícero Alves"/></p>
-            </div>
+        <div class="cicero">
+            <h4 data-compression="7" data-max="20" class="align-center zero">
+                Cícero Alves
+            </h4>
+            <p><img src="../images/cicero.jpg" alt="Cícero Alves" title="Cícero Alves"/></p>
         </div>
-        <div class="ui-block-b">
-            <div class="notice box gray square white">
-                <h4 data-compression="7" data-max="20" class="align-center zero">
-                    Raphael Willian
-                </h4>
-                <p><img src="../images/raphael.jpg" alt="Raphael Willian" title="Raphael Willian"/></p>
-            </div>
+        <div class="raphael">
+            <h4 data-compression="7" data-max="20" class="align-center zero">
+                Raphael Willian
+            </h4>
+            <p><img src="../images/raphael.jpg" alt="Raphael Willian" title="Raphael Willian"/></p>
         </div>
-        <div class="ui-block-c">
-            <div class="notice box gray square white">
-                <h4 data-compression="7" data-max="20" class="align-center zero">
-                    Rodrigo Monteiro
-                </h4>
-                <p><img src="../images/rodrigo.jpg" alt="Rodrigo Monteiro" title="Rodrigo Monteiro"/></p>
-            </div>
+        <div class="rodrigo">
+            <h4 data-compression="7" data-max="20" class="align-center zero">
+                Rodrigo Monteiro
+            </h4>
+            <p><img src="../images/rodrigo.jpg" alt="Rodrigo Monteiro" title="Rodrigo Monteiro"/></p>
         </div>
     </div>
 <?php include "../footer.php"; ?>
