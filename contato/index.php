af5382780f457e2bfb46983288b4d689331ac0df
cicerocasj <cicerocasj@hotmail.com>
2014-06-01 23:10:27 -0300

    pronto o merge com a página de contato

--- contato/index.php
+++ contato/index.php
@@ -1,21 +1,21 @@
 <?php include "../header.php"; ?>
     <div class="ui-grid-b my-breakpoint" id="contact">
         <div class="cicero">
-            <h4 data-compression="7" data-max="20" class="align-center zero">
+            <h3 data-compression="7" data-max="20" class="align-center zero">
                 Cícero Alves
-            </h4>
+            </h3>
             <p><img src="../images/cicero.jpg" alt="Cícero Alves" title="Cícero Alves"/></p>
         </div>
         <div class="raphael">
-            <h4 data-compression="7" data-max="20" class="align-center zero">
+            <h3 data-compression="7" data-max="20" class="align-center zero">
                 Raphael Willian
-            </h4>
+            </h3>
             <p><img src="../images/raphael.jpg" alt="Raphael Willian" title="Raphael Willian"/></p>
         </div>
         <div class="rodrigo">
-            <h4 data-compression="7" data-max="20" class="align-center zero">
+            <h3 data-compression="7" data-max="20" class="align-center zero">
                 Rodrigo Monteiro
-            </h4>
+            </h3>
             <p><img src="../images/rodrigo.jpg" alt="Rodrigo Monteiro" title="Rodrigo Monteiro"/></p>
         </div>
     </div>
